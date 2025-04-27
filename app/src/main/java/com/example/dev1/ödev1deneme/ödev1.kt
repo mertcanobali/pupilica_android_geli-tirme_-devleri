4ff13c7d497480260aecee1887c534dc3720b089
Mert Can OBALI <obalimertcan@gmail.com>
2025-04-27 15:46:58 +0300

    android programlama 1. ödev

---
+++ app/src/main/java/com/example/dev1/ödev1deneme/ödev1.kt
@@ -0,0 +1,143 @@
+package com.example.dev1.ödev1deneme
+
+fun main() {
+    var sehir = "İstanbul"
+    println("Şehir : $sehir")
+
+    val ulke = "TÜRKİYE"
+    println("Ülke : $ulke")
+
+    val telefon ="+90"
+    println("Telefon kodu: $telefon")
+
+    val postaKodu=34310
+    println("Posta Kodu: $postaKodu")
+
+    val email="denem@gmail.com"
+    println("Email : $email")
+
+    val meslek = "Mühendis"
+    println("Meslek: $meslek")
+
+    val musteriAdi = "Mert Can Obalı"
+    println("Müşteri Adı: $musteriAdi")
+
+    val bakiye = 000.1
+    println("Bakiye: $bakiye")
+
+    val dogumGunu = "08.04.2002"
+    println("Doğum Günü: $dogumGunu")
+
+    val maas = 0
+    println("Maaş: $maas")
+
+    val medeniDurum = "Bekar"
+    println("Medeni Durum: $medeniDurum")
+
+    val stokMiktari = 150
+    println("Stok Miktarı: $stokMiktari")
+
+    var urunYorum = "Kaliteli ürün, tavsiye ederim."
+    println("Ürün Yorum: $urunYorum")
+
+    var odemeTarihi = "25.05.2025"
+    println("Ödeme Tarihi: $odemeTarihi")
+
+    val odeme = 350.50
+    println("Ödeme: $odeme")
+
+    val siparisAdeti = 2
+    println("Sipariş Adeti: $siparisAdeti")
+
+    var arabaModeli = "Tesla"
+    println("Araba Modeli: $arabaModeli")
+
+    val kitapAdi = "Kotlin ile Programlama"
+    println("Kitap Adı: $kitapAdi")
+
+    val yayinlamaTarihi = "2020"
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+
+    val indirimMiktari = 50.0
+    println("İndirim Miktarı: $indirimMiktari")
+
+    val odaSayisi = 3
+    println("Oda Sayısı: $odaSayisi")
+
+    val urunAdi = "Laptop"
+    println("Ürün Adı: $urunAdi")
+
+    val yemekFiyati = 120.0
+    println("Yemek Fiyatı: $yemekFiyati")
+
+    val enlem = 41.0082
+    println("Enlem: $enlem")
+
+    val boylam = 28.9784
+    println("Boylam: $boylam")
+
+    val marka = "Samsung"
+    println("Marka: $marka")
+
+    val muzikAdi = "Imagine"
+    println("Müzik Adı: $muzikAdi")
+
+    val videoSuresi = 120.5
+    println("Video Süresi: $videoSuresi")
+
+    val urunPuani = 4.5
+    println("Ürün Puanı: $urunPuani")
+
+    val resimAdi = "urun_foto.jpeg"
+    println("Resim Adı: $resimAdi")
+
+    val dosyaFormati = "pdf"
+    println("Dosya Formatı: $dosyaFormati")
+
+    val renk = "Mavi"
+    println("Renk: $renk")
+
+    val renkKodu = "#0000FF"
+    println("Renk Kodu: $renkKodu")
+
+    val telefonModeli = "Galaxy S21"
+    println("Telefon Modeli: $telefonModeli")
+
+    val ekranBoyutu = 6.5
+    println("Ekran Boyutu: $ekranBoyutu")
+
+    val agirlik = 180.0
+    println("Ağırlık: $agirlik")
+
+    val ulusalGun = "29 Ekim"
+    println("Ulusal Gün: $ulusalGun")
+
+    val tatilGunu = "01 Ocak"
+    println("Tatil Günü: $tatilGunu")
+
+    val rezervasyonTarihi = "2025-05-20"
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+
+    val sokakAdi = "Mustafa Kemal Mahhallesi"
+    println("Sokak Adı: $sokakAdi")
+
+    val otobusHatti = "147"
+    println("Otobüs Hattı: $otobusHatti")
+
+    val kalanDakika = 12
+    println("Kalan Dakika: $kalanDakika")
+
+    val takipKodu = "TR12"
+    println("Takip Kodu: $takipKodu")
+
+    val kuponSuresi = 30
+    println("Kupon Süresi: $kuponSuresi gün")
+
+    val kuponKodu = "INDIRIM10"
+    println("Kupon Kodu: $kuponKodu")
+
+    val faturaAdresi = "Mustafa Kemal paşa mahhallesi no 114"
+    println("Fatura Adresi: $faturaAdresi")
+
+
+}
