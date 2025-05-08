eb8102d1c8db7f38f32fa784a4f476f989245cee
Mert Can OBALI <obalimertcan@gmail.com>
2025-05-08 18:37:16 +0300

    yeni odev 4 eklendi

---
+++ app/src/main/java/com/example/new_odev3/fragment/sayfa_bFragment.kt
@@ -0,0 +1,27 @@
+package com.example.new_odev3.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.new_odev3.R
+
+class sayfa_bFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        }
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_sayfa_b, container, false)
+        }
+
+    }
